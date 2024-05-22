43669e1d0924523adb50f1e78094cd8227e453fd
SEGARS <vim_branet@mail.ru>
2024-05-22 13:45:32 +0500

    Многопоточное программирование

---
+++ lesson_01/lesson_01.cpp
@@ -0,0 +1,41 @@
+﻿//Потоки с параметрами | Многопоточное программирование | Уроки | C++ #2 - Видео №2.
+
+#include<iostream>
+#include<string>
+#include<thread>
+
+using namespace std;
+
+//------------------------------------------------------------------------------------------
+void DoWork(int a, int b, string ms)
+{
+	cout << ms << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "========\t " << "DoWork STARTED\t=============" << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+	cout << "a+b = " << a + b << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "========\t " << "DoWork STARTED\t=============" << endl;
+}
+//------------------------------------------------------------------------------------------
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	thread th(DoWork,2,3, "or mssing");
+
+	for (size_t i = 0; true; i++)
+	{
+		cout << "ID потоков = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500) );
+	}
+
+	th.join();
+
+	return 0;
+}
+//------------------------------------------------------------------------------------------
+/*Разделение потоков данных если программа работает медленно и с большими задачами.*/
---
+++ lesson_02/lesson_02.cpp
@@ -0,0 +1,41 @@
+﻿//Возврат результата из потока по ссылке | Многопоточное программирование | Уроки | C++ #3 - Видео №3. 
+
+#include<iostream>
+#include<string>
+#include<thread>
+using namespace std;
+
+//---------------------------------------------------------------------------------------------------------
+void DoWork(int &a)
+{
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "ID потока = " << this_thread::get_id() << " =============\t " << "DoWork STARTED\t=============" << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+	a *= 2;
+
+	cout << "ID потока = " << this_thread::get_id() << " =============\t " << "DoWork STOPPED\t=============" << endl;
+}
+//---------------------------------------------------------------------------------------------------------
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	int q = 5;
+
+	thread t(DoWork, ref(q) );
+		
+	
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "ID потоков = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500) );
+	}
+	
+
+	t.join();
+	cout << q << endl;
+
+	return 0;
+}
+//---------------------------------------------------------------------------------------------------------
