b92f7b9e15015bfa8b8d2197111052eaeb7c44d9
SEGARSS <vim_branet@mail.ru>
2024-06-21 10:46:55 +0500

    Многопоточное программирование

---
+++ lesson_05/lesson_05.cpp
@@ -0,0 +1,54 @@
+// Что такое mutex | Cинхронизация потоков | Многопоточное программирование | C++ #6
+
+#include<iostream>
+#include<mutex>
+#include<thread>
+#include"SimpleTimer.h"
+
+using namespace std;
+
+
+mutex mtx;
+//-------------------------------------------------------------------------------
+void Print(char ch)
+{
+	this_thread::sleep_for(chrono::milliseconds(2000));
+
+	mtx.lock();
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			cout << ch;
+			this_thread::sleep_for(chrono::milliseconds(20));
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	mtx.unlock();
+
+	this_thread::sleep_for(chrono::milliseconds(2000));
+}
+//-------------------------------------------------------------------------------
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	SimpleTimer timer;
+
+	thread t1(Print,'*');
+	thread t2(Print,'#');
+	thread t3(Print,'@');
+
+	t1.join();
+	t2.join();
+	t3.join();
+
+	//Print('*');
+	//Print('#');
+
+	return 0;
+}
+//-------------------------------------------------------------------------------
